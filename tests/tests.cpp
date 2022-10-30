6e3cd6b35b55b4793bd7e4add23764bd4bb7cb65
ada0l <andreika.varfolomeev@yandex.ru>
2022-10-30 17:45:51 +1000

    print test report

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,9 +1,11 @@
 #include "lexer.h"
 
-int main() { TestReport report;
+int main() {
+    TestReport report;
 
     report += run_lexer_tests();
 
+    std::cout << report << "\n";
     if (report.get_failed() != 0) {
         return EXIT_FAILURE;
     }
