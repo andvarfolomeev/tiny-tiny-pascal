3bb7c8c8f53b2d04c18e675a22b89f1175310fc6
ada0l <andreika.varfolomeev@yandex.ru>
2022-11-03 12:58:47 +1000

    update

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,14 @@
 #include "lexer.h"
+#include "test_report.h"
 
 int main() {
     TestReport report;
-
     report += run_lexer_tests();
 
     std::cout << report << "\n";
     if (report.get_failed() != 0) {
         return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
