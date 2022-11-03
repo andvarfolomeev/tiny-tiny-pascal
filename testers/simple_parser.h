8885eb74b9feb65ddbb127e3a64d9b954380f3f3
ada0l <andreika.varfolomeev@yandex.ru>
2022-11-04 05:37:25 +1000

    delete old simple parser tester

--- testers/simple_parser.h
+++
@@ -1,9 +0,0 @@
-#ifndef TESTS_SIMPLE_PARSER_H
-#define TESTS_SIMPLE_PARSER_H
-
-#include "../src/scanner/scanner.h"
-#include "../src/simple_parser/simple_parser.h"
-
-void run_test_parser();
-
-#endif  // TESTS_SIMPLE_PARSER_H
