77673dce788e48ee007f40f0513ece1a96107baf
ada0l <andreika.varfolomeev@yandex.ru>
2022-12-30 03:15:07 +1000

    get_type_of_object_str for SymbolConstParam

--- src/symbol_table/symbol_var.cpp
+++ src/symbol_table/symbol_var.cpp
@@ -4,6 +4,7 @@ std::string SymbolVar::get_type_of_object_str() { return "var"; }
 std::string SymbolVar::get_ret_type_str() { return type->to_str(); }
 bool SymbolVar::is_param() { return false; }
 std::string SymbolVarParam::get_type_of_object_str() { return "var param"; }
+std::string SymbolConstParam::get_type_of_object_str() { return "const param"; }
 std::string SymbolParam::get_type_of_object_str() { return "param"; }
 bool SymbolParam::is_param() { return true; }
 std::string SymbolConst::get_type_of_object_str() { return "const"; }
