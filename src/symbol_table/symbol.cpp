a680fc82254faad5faf4ca3fc2a465700b007d79
ada0l <andreika.varfolomeev@yandex.ru>
2022-12-30 03:17:20 +1000

    [[maybe_unused]] for params in Symbol::draw_additional

--- src/symbol_table/symbol.cpp
+++ src/symbol_table/symbol.cpp
@@ -3,4 +3,5 @@
 std::string Symbol::get_name() { return name; }
 std::string Symbol::get_type_of_object_str() { return "---"; }
 std::string Symbol::get_ret_type_str() { return "---"; }
-void Symbol::draw_additional(std::ostream& os, int depth) {}
+void Symbol::draw_additional([[maybe_unused]] std::ostream& os,
+                             [[maybe_unused]] int depth) {}
