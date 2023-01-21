2ae7c07cc8a7b7bb0488dc3ca16fe3b4872b0147
ada0l <andreika.varfolomeev@yandex.ru>
2023-01-21 16:33:16 +1000

    run GeneratorTester in tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,6 +6,7 @@ int main() {
     auto res = tester::ScannerTester("../tests/scanner/").run_tests();
     res += tester::ParserTester("../tests/parser/").run_tests();
     res += tester::SemanticTester("../tests/semantic/").run_tests();
+    res += tester::GeneratorTester("../tests/generator/").run_tests();
     std::cout << res;
     return EXIT_SUCCESS;
 }
